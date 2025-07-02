8ae9bce2d35542760766069c0200394fa58fe9c6
dustygondolier <utkarsht.ec.22@nitj.ac.in>
2025-07-02 07:37:43 +0000

    updateindataconversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,7 +18,39 @@
 //"utkarsh"/1-true
 //""/0-false
 
-let someNumber = 33
-let stringsomeNumber= String(someNumber)
-console.log(stringsomeNumber)
-console.log(typeof(stringsomeNumber))
+//let someNumber = 33
+//let stringsomeNumber= String(someNumber)
+//console.log(stringsomeNumber)
+//console.log(typeof(stringsomeNumber))
+
+//------------------------------Operations------------------------------------------
+
+let value =3
+let negValue = -value
+//console.log(negValue)
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**3)//2 to power 3
+console.log(2%3)//remainder*/
+
+let str1 ="hello"
+let str2 = " utkarsh"
+let str3 = str1 + str2
+
+console.log(str3)
+
+/*console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)*/
+//all under ecma rule of guidelines
