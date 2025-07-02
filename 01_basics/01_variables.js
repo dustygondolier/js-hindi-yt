b198df3dbb32aa1d9b55d17f328dccf38260d4b4
dustygondolier <utkarsht.ec.22@nitj.ac.in>
2025-07-02 06:25:57 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId =144553
+let accountEmail ="utk@google.com"
+var accountPassword = "12345"
+accountCity= "Jaipur"
+let accountState
+
+// accountId = 2 , not allowed
+
+accountEmail ="arsh@google.com"
+var accountPassword = "45875"
+accountCity= "gorakhpur"
+
+/*prefer not to use var bcoz of issue in block scope and functional scope*/
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
