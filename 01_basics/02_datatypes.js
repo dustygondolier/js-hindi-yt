23f8294c0f7ed579ce53ced880c1fff4c1e53b0b
dustygondolier <utkarsht.ec.22@nitj.ac.in>
2025-07-02 07:01:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treats all JS code as newer version
+
+//alert(3+3)  works in browser not node.js
+
+console.log("utkarsh"); console.log(3+3) //code readable hone chahiye
+
+//alert mdn(mozilla firefox) ya tc39.es(edma script)
+
+let age =18
+let isLoggedIn = false
+let state = null
+
+//number - 2 to power 4
+//bigint
+// string - ""
+//boolean - true/false
+//null - standalone value
+//undefined - undefined
+
+//symbol - unique
+
+//object
+
+console.log(typeof "utkarsh")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof isLoggedIn)
+console.log(typeof null)
