5f945616eb0543454a749e2b949ef94ea42d118e
dustygondolier <utkarsht.ec.22@nitj.ac.in>
2025-07-02 07:18:44 +0000

    dataconversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+//let score = true
+
+//console.log(typeof(score))
+
+//let valueInNumber= Number(score)//converted in number
+//console.log(typeof( valueInNumber))
+//console.log(valueInNumber)
+
+//"33" - 33
+// null - 0
+// true/false - 0/1 
+//undefined/33abc- NaN
+
+//let isLoggedIn =  0
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+//"utkarsh"/1-true
+//""/0-false
+
+let someNumber = 33
+let stringsomeNumber= String(someNumber)
+console.log(stringsomeNumber)
+console.log(typeof(stringsomeNumber))
