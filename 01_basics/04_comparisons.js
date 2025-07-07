b6d58c20e202efff7aeb1196cef3bfea9ec6aff4
dustygondolier <utkarsht.ec.22@nitj.ac.in>
2025-07-07 09:58:23 +0000

    comparisons in JS

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,22 @@
+// console.log( 2 >= 1);
+// console.log( 2 > 1);
+// console.log( 2 < 1);
+// console.log( 2 != 1);
+
+//predictable result ni deta aise
+// console.log("2">1);
+// console.log("02">=1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0); //jab bhi comparator use hota null ko 0 ki trh treat kra jaata brna null is alg quantity than 0;
+
+console.log(undefined >0);
+ console.log(undefined ==0);
+console.log(undefined >=0);
+
+// strict check (===) , checks the datatype
+
+console.log("2"==2);
+console.log("2"===2);
+
